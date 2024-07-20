06131cb318692faba2b1fd84fee16b299eb554c9
Gustavo Moitinho Trindade <gmoitinhotrindade@gmail.com>
2024-07-19 21:49:10 -0300

    Add new neotree setting

--- init.lua
+++ init.lua
@@ -21,3 +21,13 @@ require('lspconfig').ltex.setup({
     vim.cmd("set nospell")
   end,
 })
+
+require('neo-tree').setup({
+  filesystem = {
+    filtered_items = {
+      visible = true, -- When this is set to true, hiding just means making it dim
+      hide_dotfiles = false, -- So this will show fully visible
+      hide_gitignored = true, -- And this will be dimmed out
+    }
+  }
+})
